a5f36c0fe4c88ec201aa8d58f6c938c02bd595d4
Oversv <albertomdv@gmail.com>
2020-03-04 12:28:37 +0000

    Updates

---
+++ precache-manifest.13a19d0feeb2a025edfc93ae4245d41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599087e393262bd4df09cda4c290d986",
+    "url": "/freeCodeCamp-Markdown_Previewer/index.html"
+  },
+  {
+    "revision": "267368acaf65b90487dd",
+    "url": "/freeCodeCamp-Markdown_Previewer/static/css/main.6fa41f00.chunk.css"
+  },
+  {
+    "revision": "0660a726cfcd28f3fd96",
+    "url": "/freeCodeCamp-Markdown_Previewer/static/js/2.181d64ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/freeCodeCamp-Markdown_Previewer/static/js/2.181d64ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267368acaf65b90487dd",
+    "url": "/freeCodeCamp-Markdown_Previewer/static/js/main.5a2c5332.chunk.js"
+  },
+  {
+    "revision": "73a6405b1d10e86b9768",
+    "url": "/freeCodeCamp-Markdown_Previewer/static/js/runtime-main.37b8e4d5.js"
+  }
+]);
